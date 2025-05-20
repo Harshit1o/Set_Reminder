878fe9b230ef865f0ec78c9c00d4d071fe206b69
Harshit <164295315+Harshit1o@users.noreply.github.com>
2025-05-20 23:34:44 +0530

    Update views.py

--- reminders/views.py
+++ reminders/views.py
@@ -6,8 +6,6 @@ from drf_yasg import openapi
 from .models import Reminder
 from .serializers import ReminderSerializer
 
-# Create your views here.
-
 class ReminderViewSet(viewsets.ModelViewSet):
     queryset = Reminder.objects.all()
     serializer_class = ReminderSerializer
