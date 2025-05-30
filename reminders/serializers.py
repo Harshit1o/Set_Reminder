1758799e4111a0546babc891b4dae78a38a7c3f5
Harshit <164295315+Harshit1o@users.noreply.github.com>
2025-05-30 19:19:40 +0530

    Update serializers.py

--- reminders/serializers.py
+++ reminders/serializers.py
@@ -6,4 +6,4 @@ class ReminderSerializer(serializers.ModelSerializer):
         model = Reminder
         fields = ['id', 'title', 'message', 'reminder_date', 'reminder_time', 
                   'reminder_type', 'recipient', 'created_at', 'is_sent']
-        read_only_fields = ['id', 'created_at', 'is_sent']
+        read_only_fields = ['id', 'created_at']
