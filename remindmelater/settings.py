42ba55561504b5fd91e1d267c9a879bf368bfe44
Harshit <164295315+Harshit1o@users.noreply.github.com>
2025-06-06 23:35:54 +0530

    Update settings.py

--- remindmelater/settings.py
+++ remindmelater/settings.py
@@ -16,10 +16,6 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
